64381a7c6dd10aa5107b977a2f328e44885ba4ba
khaireddine krichene <trabelsinajiba1997@gmail.com>
2021-06-22 18:24:33 +0100

    check users

--- sang_plus_plus_admin/lib/screens/confirm_perso.dart
+++ sang_plus_plus_admin/lib/screens/confirm_perso.dart
@@ -92,12 +92,11 @@ class _ConfirmPersoState extends State<ConfirmPerso> {
                       if (gs == '') {
                         setState(() {
                           erreur =
-                              'le champ confirmer groupe sanguin est obligatoire ';
+                              'le champ confirmer groupe sanguin est obligatoire';
                         });
                       } else if (sexe == '') {
                         setState(() {
-                          erreurOne =
-                              'le champ confirmer sexe est obligatoire ';
+                          erreurOne = 'le champ confirmer sexe est obligatoire';
                         });
                       } else {
                         Navigator.pop(context);
